a5d3b55c1cd3a5501f74064336f0869d950eabae
joe <josephjtrovato@gmail.com>
2015-04-20 23:19:33 -0400

    first commit

---
+++ GetInliersRANSAC.m
@@ -0,0 +1,11 @@
+function [y1, y2, idx] = GetInliersRANSAC(x1, x2)
+%use ransac to get inlier point between two sets of matched features
+
+maxiters = 10000;
+
+
+
+
+
+end
+
---
+++ proj2.m
@@ -0,0 +1,28 @@
+%Structure from motion
+
+%% Parse the Data
+
+matches = cell(5,6);
+
+mdir = dir('./matching');
+for i=3:length(mdir)
+    fprintf([mdir(i).name, '\n']);
+    data = dlmread(['./matching/', mdir(i).name], ' ', 1 ,0);
+    for j=1:length(data)
+        for k=1:data(j, 1)-1
+           matches{i-2, data(j,7+(k-1)*3)} = [matches{i-2, data(j,7+(k-1)*3)} ; [data(j,5:6), data(j, (7+(k-1)*3+1):(7+(k-1)*3+2))]];
+        end
+    end
+end
+
+
+
+%% Reject Outliers with RANSAC
+
+numimages = length(mdir)-2;
+for i=1:numimages
+    for j = i+1:numimages
+        cur_m = matches{i, j};
+        GetInliersRANSAC(cur_m(:,1:2), cur_m(:,3:4))
+    end
+end
